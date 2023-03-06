7f22966389c44ebf35ea3ffba629e94ee593d675
maisamrr <maisammoreira@gmail.com>
2023-03-06 19:01:27 -0300

    aula de arrays

--- src/javautil/TesteArrayDeReferencias.java
+++ src/javautil/TesteArrayDeReferencias.java
@@ -1,8 +1,20 @@
 package javautil;
 
+import bytebankherdado.ContaCorrente;
+
 public class TesteArrayDeReferencias {
 
     public static void main(String[] args) {
+
+        ContaCorrente[] contas = new ContaCorrente[5];
+
+        ContaCorrente cc1 = new ContaCorrente(666, 90087);
+        contas[0] = cc1;
+        ContaCorrente cc2 = new ContaCorrente(666, 90527);
+        contas[1] = cc2;
+
+        System.out.println("CC1: Agencia = " + cc1.getAgencia() + " e Conta = " + cc1.getNumero());
+        System.out.println(contas[0].getNumero());
 
     }
 }
---
+++ src/javautil/TesteArrayString.java
@@ -0,0 +1,12 @@
+package javautil;
+
+public class TesteArrayString {
+    public static void main(String[] args) {
+
+        System.out.println("Funcionouu :)");
+
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
